6ace37e858392cf180104cf1f3555230911d0575
Eric Chinen <7997656+echinen@users.noreply.github.com>
2023-10-04 16:35:07 -0300

    set up all routes in main file

---
+++ backend/app/routes/index.js
@@ -0,0 +1,21 @@
+const userRoute = require('./routes/userRoute')
+const teacherRoute = require('./routes/teacherRoute')
+const studentRoute = require('./routes/studentRoute')
+const schoolRoute = require('./routes/schoolRoute')
+const registrationRoute = require('./routes/registrationRoute')
+const redFlagRoute = require('./routes/redFlagRoute')
+const mappingRoute = require('./routes/mappingRoute')
+const disorderTypeRoute = require('./routes/disorderTypeRoute')
+const classRoute = require('./routes/classRoute')
+
+module.exports = {
+    userRoute,
+    teacherRoute,
+    studentRoute,
+    schoolRoute,
+    registrationRoute,
+    redFlagRoute,
+    mappingRoute,
+    disorderTypeRoute,
+    classRoute
+}
--- backend/app/server.js
+++ backend/app/server.js
@@ -1,32 +1,50 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const config = require("./config");
-const sequelize = require("./database/connection");
-const userRoute = require("./routes/userRoute");
+const config = require('./config')
+const sequelize = require('./database/connection')
+const { 
+  classRoute,
+  disorderTypeRoute,
+  mappingRoute,
+  redFlagRoute,
+  registrationRoute,
+  schoolRoute,
+  studentRoute,
+  teacherRoute,
+  userRoute
+} = require('./routes/index')
 
-const port = config.server.port;
+
+const port = config.server.port
 
 // Add all middlewares here
 app.use(express.json());
 
 // All public routes - check health
-app.get("/", (req, res) => {
-  res.send("Bem-vindo à minha API!");
+app.get('/', (req, res) => {
+  res.send('Bem-vindo à minha API!');
 });
 
 // all private routes
-app.use("/api", userRoute);
+app.use('/api', classRoute)
+app.use('/api', disorderTypeRoute)
+app.use('/api', mappingRoute)
+app.use('/api', redFlagRoute)
+app.use('/api', registrationRoute)
+app.use('/api', schoolRoute)
+app.use('/api', studentRoute)
+app.use('/api', teacherRoute)
+app.use('/api', userRoute)
 
 // sync to the database
-sequelize
-  .sync()
-  .then((result) => {
-    console.log(result);
-    // create server
-    app.listen(port, () => {
-      console.log(`A API está rodando na porta ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.log(error);
+sequelize.sync()
+.then(result => {
+  console.log(result)
+  // create server
+  app.listen(port, () => {
+    console.log(`A API está rodando na porta ${port}`);
   });
+})
+.catch(error => {
+  console.log(error)
+})
