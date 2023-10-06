6707042c7a8d2de4b84e7daf2f3ee8cc08323960
ariany-f <ariany_f@hotmail.com>
2023-10-05 23:56:54 -0300

    sair

--- frontend/index.js
+++ frontend/index.js
@@ -15,4 +15,5 @@ function logout()
     document.cookie.split(';').forEach(function(c) {
         document.cookie = c.trim().split('=')[0] + '=;' + 'expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/';
     });
+    window.location.href = '/auth';
 }
